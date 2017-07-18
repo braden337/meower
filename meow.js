8515b493389f1ae9e88714006c0e76f7b1984e90
Braden Jonsson <braden337@gmail.com>
2017-07-17 23:18:03 -0500

    added space after :cat:

--- meow.js
+++ meow.js
@@ -6,6 +6,6 @@ for (let i = 0; i < 10; i++) {
   for (let meow = 0; meow < meow_num; meow++)
     meows.push('meow')
   if (i % 2 == 0) meows.unshift(' ')
-  meows = meows.join('🐱')
+  meows = meows.join('🐱 ')
   console.log(meows)
 }
